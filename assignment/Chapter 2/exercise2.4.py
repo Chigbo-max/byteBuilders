405fa96681b9faef1b49996c56b0c3b506404bb9
Chigbo <ezeokeke.chigbo@gmail.com>
2024-08-31 19:09:24 +0100

    chapter 2 assignments

---
+++ assignment/Chapter 2/exercise2.4.py
@@ -0,0 +1,13 @@
+addition = 27.5 + 2
+subtraction = 27.5 - 2
+multiplication = 27.5 * 2
+true_division = 27.5 / 2
+floor_division = 27.5 // 2
+exponentiation = 27.5 ** 2
+
+print("addition = ", addition)
+print("subtraction = ", subtraction)
+print("multiplication = ", multiplication)
+print("true_division = ", true_division)
+print("floor_division = ", floor_division)
+print("exponentiation = ", exponentiation)
---
+++ assignment/Chapter 2/exercise2.6.py
@@ -0,0 +1,8 @@
+user_input = int(input("Enter a number: "))
+MULTIPLE = 2
+
+if(user_input % MULTIPLE == 0):
+	print(user_input, "is an even number")
+else:
+
+	print(user_input, "is an odd number")

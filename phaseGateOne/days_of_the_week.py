ab8d828bd5422fe19125066b538ff21b266be8de
Chigbo <ezeokeke.chigbo@gmail.com>
2024-09-30 15:18:56 +0100

    days_of_the_week.py

---
+++ phaseGateOne/days_of_the_week.py
@@ -0,0 +1,151 @@
+todays_day = int(input("Enter today's day: "))
+number_of_days_after_today = int(input("Enter future day: "))
+
+sunday = 0
+monday = 1
+tuesday = 2
+wednesday = 3
+thursday = 4
+friday = 5
+saturday = 6
+
+future_sunday = 0
+
+if(todays_day == 0 and number_of_days_after_today == 1 ):
+	print("Today is sunday and the future day is Monday")
+
+if(todays_day == 0 and number_of_days_after_today == 2 ):
+	print("Today is sunday and the future day is Tuesday")
+
+if(todays_day == 0 and number_of_days_after_today == 3 ):
+	print("Today is sunday and the future day is Wednesday")
+
+if(todays_day == 0 and number_of_days_after_today == 4 ):
+	print("Today is sunday and the future day is Thursday")
+
+if(todays_day == 0 and number_of_days_after_today == 5 ):
+	print("Today is sunday and the future day is Friday")
+
+if(todays_day == 0 and number_of_days_after_today == 6 ):
+	print("Today is sunday and the future day is Saturday")
+
+
+
+if(todays_day == 1 and number_of_days_after_today == 1 ):
+	print("Today is monday and the future day is Tuesday")
+
+if(todays_day == 1 and number_of_days_after_today == 2 ):
+	print("Today is monday and the future day is Wednesday")
+
+if(todays_day == 1 and number_of_days_after_today == 3 ):
+	print("Today is monday and the future day is Thursday")
+
+if(todays_day == 1 and number_of_days_after_today == 4 ):
+	print("Today is monday and the future day is Friday")
+
+if(todays_day == 1 and number_of_days_after_today == 5 ):
+	print("Today is monday and the future day is Saturday")
+
+if(todays_day == 1 and number_of_days_after_today == 6 ):
+	print("Today is monday and the future day is Sunday")
+
+
+
+if(todays_day == 2 and number_of_days_after_today == 1 ):
+	print("Today is tuesday and the future day is Wednesday")
+
+if(todays_day == 2 and number_of_days_after_today == 2 ):
+	print("Today is tuesday and the future day is Thursday")
+
+if(todays_day == 2 and number_of_days_after_today == 3 ):
+	print("Today is tuesday and the future day is Friday")
+
+if(todays_day == 2 and number_of_days_after_today == 4 ):
+	print("Today is tuesday and the future day is Saturday")
+
+if(todays_day == 2 and number_of_days_after_today == 5 ):
+	print("Today is tuesday and the future day is Sunday")
+
+if(todays_day == 2 and number_of_days_after_today == 6 ):
+	print("Today is tuesday and the future day is Monday")
+
+
+
+if(todays_day == 3 and number_of_days_after_today == 1 ):
+	print("Today is wednesday and the future day is Thursday")
+
+if(todays_day == 3 and number_of_days_after_today == 2 ):
+	print("Today is wednesday and the future day is Friday")
+
+if(todays_day == 3 and number_of_days_after_today == 3 ):
+	print("Today is wednesday and the future day is Saturday")
+
+if(todays_day == 3 and number_of_days_after_today == 4 ):
+	print("Today is wednesday and the future day is Sunday")
+
+if(todays_day == 3 and number_of_days_after_today == 5 ):
+	print("Today is wednesday and the future day is Monday")
+
+if(todays_day == 3 and number_of_days_after_today == 6 ):
+	print("Today is wednesday and the future day is Tuesday")
+
+
+if(todays_day == 4 and number_of_days_after_today == 1 ):
+	print("Today is thursday and the future day is Friday")
+
+if(todays_day == 4 and number_of_days_after_today == 2 ):
+	print("Today is thursday and the future day is Saturday")
+
+if(todays_day == 4 and number_of_days_after_today == 3 ):
+	print("Today is thursday and the future day is Sunday")
+
+if(todays_day == 4 and number_of_days_after_today == 4 ):
+	print("Today is thursday and the future day is Monday")
+
+if(todays_day == 4 and number_of_days_after_today == 5 ):
+	print("Today is thursday and the future day is Tuesday")
+
+if(todays_day == 4 and number_of_days_after_today == 6 ):
+	print("Today is thursday and the future day is Wednesday")
+
+
+if(todays_day == 5 and number_of_days_after_today == 1 ):
+	print("Today is friday and the future day is Saturday")
+
+if(todays_day == 5 and number_of_days_after_today == 2 ):
+	print("Today is friday and the future day is Sunday")
+
+if(todays_day == 5 and number_of_days_after_today == 3 ):
+	print("Today is friday and the future day is Monday")
+
+if(todays_day == 5 and number_of_days_after_today == 4 ):
+	print("Today is friday and the future day is Tuesday")
+
+if(todays_day == 5 and number_of_days_after_today == 5 ):
+	print("Today is friday and the future day is Wednesday")
+
+if(todays_day == 5 and number_of_days_after_today == 6 ):
+	print("Today is friday and the future day is Thursday")
+
+
+
+
+if(todays_day == 6 and number_of_days_after_today == 1 ):
+	print("Today is Saturday and the future day is Sunday")
+
+if(todays_day == 6 and number_of_days_after_today == 2 ):
+	print("Today is Saturday and the future day is Monday")
+
+if(todays_day == 6 and number_of_days_after_today == 3 ):
+	print("Today is Saturday and the future day is Tuesday")
+
+if(todays_day == 6 and number_of_days_after_today == 4 ):
+	print("Today is Saturday and the future day is Wednesday")
+
+if(todays_day == 6 and number_of_days_after_today == 5 ):
+	print("Today is Saturday and the future day is Thursday")
+
+if(todays_day == 6 and number_of_days_after_today == 6 ):
+	print("Today is Saturday and the future day is Friday")
+
+

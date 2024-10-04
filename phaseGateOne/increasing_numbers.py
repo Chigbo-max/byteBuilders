b5801e3c09241fa1597a1f2f48933fa604e056c9
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-04 04:29:07 +0100

    update on tasks

--- phaseGateOne/increasing_numbers.py
+++ phaseGateOne/increasing_numbers.py
@@ -6,28 +6,14 @@ for _ in range(0,number_of_elements):
 	number = int(input("Enter an integer: "))
 	array.append(number)
 
-print(sorted(array))
-print(sorted(array, reverse = True))
-
-
-number1 = int(input("Enter number: "))
-number2 = int(input("Enter number: "))
-number3 = int(input("Enter number: "))
-
-if number1 > number2:
-	number1,number2 = number2,number1
-if number1 > number3:
-	number1, number3 = number3,number1
-
-if number2 > number3:
-	number2, number3 = number3, number2
-
-
-
-print(number1, number2, number3)
-
-
-
+for index in range(0, len(array)):
+	for checker in range(1, len(array)):
+		if(array[checker-1] > array[checker]):
+			temp = array[checker-1]
+			array[checker-1] = array[checker]
+			array[checker] = temp
+	
+print(array)
 
 
 

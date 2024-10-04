bde7b03021aa8369351e93cc1d644cb0cc5abdb3
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-04 16:28:28 +0100

    updated tasks

--- PhaseGateTwo/listoftennumbers.py
+++ PhaseGateTwo/listoftennumbers.py
@@ -5,7 +5,7 @@ even_sum = 0
 odd_sum = 0
 multiplication = 1
 average = 0
-
+length = 0
 
 for numbers in range(10):
 	array.append(random.randrange(50))
@@ -21,6 +21,9 @@ for numbers in range(10):
 
 	average = array[numbers] // 10
 
+	length = length +1
+	
+
 	
 
 
@@ -28,4 +31,5 @@ print(even_sum)
 print(odd_sum)
 print(multiplication)
 print(average)
+print(length)
 

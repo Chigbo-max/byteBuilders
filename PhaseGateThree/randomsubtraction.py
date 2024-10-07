7a6c007c8e1eb7717baab2800d709c69f35df0f5
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-07 18:11:22 +0100

    update

--- PhaseGateThree/randomsubtraction.py
+++ PhaseGateThree/randomsubtraction.py
@@ -1,21 +1,22 @@
 import random
 
-number = int(input("Enter a number: "))
-count = 0
-def display_random_subtraction(number):
+def display_random_subtraction():
+	count = 0
 	for value in range(10):
 		firstRandom = 1 + random.randrange(100)
 		secondRandom = 1 + random.randrange(100)
 	
 		subtraction = secondRandom - firstRandom
+		
+		number = int(input("Enter a number: "))
+		count+=1
 
 		if(number == subtraction):
 			print("Correct")
 		else:
 			print("Incorrect")
-		
-		count+=1
-print(display_random_subtraction(number))
+
+display_random_subtraction()
 
 
 

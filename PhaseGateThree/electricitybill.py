ec474f68c2812220e7037d55ffe8c4381cadac29
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-08 07:40:11 +0100

    updated tasks

--- PhaseGateThree/electricitybill.py
+++ PhaseGateThree/electricitybill.py
@@ -1,8 +1,17 @@
-unit = int(input("Enter number of units: "))
+try:
+	unit = int(input("Enter number of units: "))
 
-price = 0
-if (unit > 100 and unit <= 200):
-	price = (unit - 100) * 50
-elif(unit > 200):
-	price = (unit - 100) * 50
-print(price)
+	price = 0
+
+	if(unit < 0):
+		price = unit * 50
+	if(unit > 0 and unit <=100):
+		print("no charge")
+	if (unit > 100 and unit <= 200):
+		price =  (unit -100) * 50
+	elif(unit > 200):
+		price = (100*50) + (unit - 200) *100
+	print(price)
+
+except ValueError:
+	print("invalid input")
--- PhaseGateThree/test.py
+++ PhaseGateThree/test.py
@@ -1,6 +1,16 @@
+
 a = 9
+b = 6
+c = 12
+d= 15
 
 if a > 5 and a <=10:
 	print("hello")
 else:
 	print("bye")
+
+if a > b and c > d
+	print("hello")
+else:
+	print("bye")
+

5749bf0c895874203a43f564914b9d6c9eddbbd6
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-11 17:04:28 +0100

    updated tasks

--- phaseGateFour/luckynumbers.py
+++ phaseGateFour/luckynumbers.py
@@ -21,21 +21,21 @@ if(words[0] == first_random and words[1] == second_random and words[2] == third_
 elif(words[0] == second_random and words[1] == third_random and words[2] == first_random): print("You've won $4,000! You're close to Gold!")
 
 
-elif(words[0] == third_random and words[1] == first_random and words[2] == second_random): print("You've won $4,000!, You're close to Gold!")
+elif(words[0] == third_random and words[1] == first_random and words[2] == second_random): print("You've won $4,000! You're close to Gold!")
 
-elif(words[0] == first_random and words[1] != second_random and words[2] == third_random): print("You've won $3,000, but you can do better!")
+elif(words[0] == first_random and words[1] != second_random and words[2] == third_random): print("You've won $3,000 but you can do better!")
 
-elif(words[0] == first_random and words[1] != second_random and words[2] != third_random): print("You've won $3,000, but you can do better!")
+elif(words[0] == first_random and words[1] != second_random and words[2] != third_random): print("You've won $3,000 but you can do better!")
 
-elif(words[0] != first_random and words[1] == second_random and words[2] == third_random): print("You've won $3,000, but you can do better!")
+elif(words[0] != first_random and words[1] == second_random and words[2] == third_random): print("You've won $3,000 but you can do better!")
 
-elif(words[0] == first_random and words[1] == second_random and words[2] != third_random): print("You've won $3,000, but you can do better!")
+elif(words[0] == first_random and words[1] == second_random and words[2] != third_random): print("You've won $3,000 but you can do better!")
 
-elif(words[0] != first_random and words[1] != second_random and words[2] == third_random): print("You've won $2,000, but you can do better!")
+elif(words[0] != first_random and words[1] != second_random and words[2] == third_random): print("You've won $2,000 but you can do better!
 
-elif(words[0] == first_random and words[1] != second_random and words[2] != third_random): print("You've won $2,000, but you can do better!")
+elif(words[0] == first_random and words[1] != second_random and words[2] != third_random): print("You've won $2,000 but you can do better!")
 
-elif(words[0] != first_random and words[1] == second_random and words[2] != third_random): print("You've won $2,000, but you can do better!")
+elif(words[0] != first_random and words[1] == second_random and words[2] != third_random): print("You've won $2,000 but you can do better!")
 
 elif (words[0] != first_random and words[1] != second_random and words[2] != third_random): print("oops, nice try! Give it your best shot next time!")
 

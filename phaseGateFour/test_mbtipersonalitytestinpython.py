15c94cb5667b71a988d1a6336823586b9c64216b
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-21 02:47:51 +0100

    mbit personality python task

--- phaseGateFour/test_mbtipersonalitytestinpython.py
+++ phaseGateFour/test_mbtipersonalitytestinpython.py
@@ -4,7 +4,7 @@ import mbtipersonalitytestinpython
 class TestMbtiPersonalityTest(unittest.TestCase):
 		def test_that_get_user_input_function_exist(self):
 
-			survey = [[0]*2 for _ in range(20)]
+			survey = [[""]*2 for _ in range(20)]
 			mbtipersonalitytestinpython.get_user_input(survey)
 		
 		def test_that_get_user_input_receives_correct_input(self):
@@ -14,7 +14,7 @@ class TestMbtiPersonalityTest(unittest.TestCase):
 			self.assertEqual(mbtipersonalitytestinpython.get_user_input(character), expected)
 		
 		def test_that_display_introverts_vs_extroverts_results_function_exist(self):
-			survey = [""*2 for _ in range(20)]
+			survey = [[""]*2 for _ in range(20)]
 
 			options= [["A","B"]]*20
 		
@@ -22,14 +22,14 @@ class TestMbtiPersonalityTest(unittest.TestCase):
 
 			name = "interesting"
 
-			personality_type = [0]*4
+			personality_type = [""]*4
 		
 
 			mbtipersonalitytestinpython.display_introverts_vs_extroverts_results(answers, survey, options, name, personality_type)
 
 
 		def test_that_display_introverts_vs_extroverts_results_function_returns_result(self):
-			survey = [[0]*2 for _ in range(20)]
+			survey = [[""]*2 for _ in range(20)]
 
 			options= [["A","B"]]*20
 		
@@ -37,12 +37,12 @@ class TestMbtiPersonalityTest(unittest.TestCase):
 
 			name = "interesting"
 
-			personality_type = [0]*4
+			personality_type = [""]*4
 		
 
 			expected = "I"
-		
-			self.assertEqual(mbtipersonalitytestinpython.display_introverts_vs_extroverts_results(answers, survey, options, name, personality_type), expected)
+			mbtipersonalitytestinpython.display_introverts_vs_extroverts_results(answers, survey, options, name, personality_type)
+			self.assertEqual(personality_type[0], expected)
 
 	
 	

f09021c5446b5848d460d47931b6e9ddb298d1cf
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-21 02:57:12 +0100

    mbit tasks

--- phaseGateFour/test_mbtipersonalitytestinpython.py
+++ phaseGateFour/test_mbtipersonalitytestinpython.py
@@ -44,5 +44,52 @@ class TestMbtiPersonalityTest(unittest.TestCase):
 			mbtipersonalitytestinpython.display_introverts_vs_extroverts_results(answers, survey, options, name, personality_type)
 			self.assertEqual(personality_type[0], expected)
 
+
+		def test_that_display_sensing_vs_intuitive_results_function_returns_result(self):
+			survey = [[""]*2 for _ in range(20)]
+
+			options= [["A","B"]]*20
+		
+			answers = ["A","B","A","B","A","A","B","A","B","A","B","A","B","A","B","A","B","A","B","A"]
+
+
+			personality_type = [""]*4
+		
+
+			expected = "N"
+			mbtipersonalitytestinpython.display_sensing_vs_intuitive_results(answers, survey, options, personality_type)
+			self.assertEqual(personality_type[1], expected)
+
+
+		def test_that_display_thinking_vs_feeling_results_function_returns_result(self):
+			survey = [[""]*2 for _ in range(20)]
+
+			options= [["A","B"]]*20
+		
+			answers = ["A","B","A","B","A","A","B","A","B","A","B","A","B","A","B","A","B","A","B","A"]
+
+
+			personality_type = [""]*4
+		
+
+			expected = "F"
+			mbtipersonalitytestinpython.display_thinking_vs_feeling_results(answers, survey, options, personality_type)
+			self.assertEqual(personality_type[2], expected)
+
+
+
+		def test_that_display_judging_vs_perceptive_inputs_function_returns_result(self):
+			survey = [[""]*2 for _ in range(20)]
+
+			options= [["A","B"]]*20
+		
+			answers = ["A","B","A","B","A","A","B","A","B","A","B","A","B","A","B","A","B","A","B","A"]
+
+			personality_type = [""]*4
+		
+
+			expected = "P"
+			mbtipersonalitytestinpython.display_judging_vs_perceptive_inputs(answers, survey, options, personality_type)
+			self.assertEqual(personality_type[3], expected)
 	
 	

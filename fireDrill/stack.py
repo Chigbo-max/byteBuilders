9521fd78b56a5de20b590384b8a76d525a83c226
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2024-11-25 07:25:58 +0100

    stack, queue updated task

--- fireDrill/stack.py
+++ fireDrill/stack.py
@@ -23,3 +23,27 @@ class Stack:
 
     def isFull(self):
         return self.size == len(self.items)
+
+class Queue:
+    def __init__(self, size):
+        self.size = size
+        self.items = []
+
+    def empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        if self.isFull(): raise ValueError('Queue is full')
+        return self.items.append(item)
+
+    def getItems(self):
+        return self.items
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def front(self):
+        return self.items[0]
+
+    def isFull(self):
+        return self.size == len(self.items)
